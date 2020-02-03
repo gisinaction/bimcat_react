b69cdc46b665aca882f1e41685633f261f5f0d69
254033230@qq.com <254033230@qq.com>
2020-02-03 17:49:41 +0800

    初始化项目。

---
+++ config/config.js
@@ -0,0 +1,31 @@
+export default {
+    singular: true,
+    history:'hash', //保留刷新前的配置
+    plugins: [
+        ['umi-plugin-react', {
+            antd: true
+        }],
+      ],
+      routes: [{
+        path: '/',
+        component: '../layout',
+        routes: [
+          {
+            path: '/',
+            component: 'Helloworld',
+          },
+          {
+            path: '/helloworld',
+            component: 'Helloworld'
+          },
+          {
+            path: '/dashboard',
+            routes: [
+              { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+              { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+              { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+            ]
+          },
+        ]
+      }]
+};
